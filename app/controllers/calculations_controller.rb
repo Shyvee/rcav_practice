931182aaa5f2a9cbbfc2c7ade90a569fd63d7ab8
Shyvee <xshi2016@kellogg.northwestern.edu>
2015-05-10 17:13:07 -0500

    little calculator updated
    little calculator updated

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,22 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+  def square
+        @numbers=params[:id].to_i
+        @square=@numbers** 2.to_i
+  end
+
   def sqrt
+      @numbers=params[:id].to_i
+      @sqrt=Math.sqrt(@numbers).to_i
   end
 
   def pmt
+      @apr=params[:INTEREST_RATE].to_f
+      @rate=@apr/100/12
+      @nper=params[:NUMBER_OF_PAYMENTS].to_i
+      @principal=params[:PRINCIPAL_VALUE].to_f
+      @payment=@rate*@principal/(1-(1+@rate)**-@nper)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+
+  get("/square/:id",  { :controller => "calculations", :action => "square" })
+
+  get("/square_root/:id",   { :controller => "calculations", :action => "sqrt" })
+
+
+  get("/payment/:INTEREST_RATE/:NUMBER_OF_PAYMENTS/:PRINCIPAL_VALUE", { :controller => "calculations", :action => "pmt" })
+
 end
